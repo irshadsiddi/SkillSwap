e3a73590daaebcd54c442b05b12969ddfb567d76
GANAPATHRI PRAPHUL CHANDRA <156814647+praphulchandra-nitdgp@users.noreply.github.com>
2025-07-12 12:13:37 +0530

    Delete Frontend directory

--- Frontend/main.js
+++
@@ -1,6 +0,0 @@
-fetch('http://localhost:5000/api/users/public')
-  .then(res => res.json())
-  .then(data => {
-    const div = document.getElementById('users');
-    div.innerHTML = data.map(user => `<p>${user.name} - Skills: ${user.skillsOffered.join(', ')}</p>`).join('');
-  });
